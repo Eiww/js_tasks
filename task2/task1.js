0757869cb740ddbe71c46fd9c6ed4dd7a16b94a0
Eiww <niktonikakoj0@gmail.com>
2024-01-26 08:40:48 +0900

    update task 2

---
+++ task2/task1.js
@@ -0,0 +1,20 @@
+const age = 18;
+const name = "yulia";
+const hasDog = true;
+const hasBoyfriend = null;
+const address = undefined;
+const pasport = {
+  seria: 78756,
+  number: 13515,
+};
+const snils = 234567898765432345678876n;
+const id = Symbol("1456215");
+
+console.log("age", age, typeof age);
+console.log("name", name, typeof name);
+console.log("hasDog", hasDog, typeof hasDog);
+console.log("hasBoyfriend", hasBoyfriend, typeof hasBoyfriend);
+console.log("address", address, typeof address);
+console.log("pasport", pasport, typeof pasport);
+console.log("snils", snils, typeof snils);
+console.log("id", id, typeof id);
---
+++ task2/task2/index.js
@@ -0,0 +1,10 @@
+let age = 20;
+let name = "yulia";
+let hasDog = false;
+
+age = "18";
+alert(age);
+name = 4;
+alert(name);
+hasDog = null;
+alert(hasDog);
