3385c2a014893c56719a6d64ff3ca72f06bb7d61
Eiww <niktonikakoj0@gmail.com>
2024-01-26 10:20:33 +0900

    update task 3

---
+++ task3/ts1/index.js
@@ -0,0 +1,20 @@
+const age = 18;
+const name = "yulia";
+const hasDog = true;
+const hasBoyfriend = null;
+const address = undefined;
+const pasport = {
+  seria: 78756,
+  number: 13515,
+};
+const snils = 234567898765432345678876n;
+const id = Symbol("1456215");
+
+console.log(Number(age), Boolean(age), String(age));
+console.log(Number(name), Boolean(name), String(name));
+console.log(Number(hasDog), Boolean(hasDog), String(hasDog));
+console.log(Number(hasBoyfriend), Boolean(hasBoyfriend), String(hasBoyfriend));
+console.log(Number(address), Boolean(address), String(address));
+console.log(Number(pasport), Boolean(pasport), String(pasport));
+console.log(Number(snils), Boolean(snils), String(snils));
+console.log(Number(id), Boolean(id), String(id));
---
+++ task3/ts2/task-2.js
@@ -0,0 +1,32 @@
+console.log(Number("10"), typeof Number("10"));
+// явное
+
+console.log(+"50");
+//неявное
+
+console.log(Number("100"), typeof Number("100"));
+//явное
+
+console.log("" + 1, typeof "" + 1);
+//неявное
+
+console.log(String(1), typeof String(1));
+//явное
+
+console.log(Boolean(0), typeof Boolean(0));
+//явное
+
+console.log(+"1001", typeof +"1001");
+//неявное
+
+console.log(1 + "", typeof 1 + "");
+//неявное
+
+console.log(Boolean(1), typeof Boolean(1));
+//явное
+
+console.log(String(1), typeof String(1));
+//явное
+
+console.log(Number("Hello World"), typeof Number("Hello World"));
+//явное
