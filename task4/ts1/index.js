7415eced482c9ca62ff2faf069f0a84cbd94433d
Eiww <niktonikakoj0@gmail.com>
2024-01-30 11:17:12 +0900

    start task 4

---
+++ task4/ts1/index.js
@@ -0,0 +1,12 @@
+const myName = "Юлия";
+const progammingLanguage = "JavaScript";
+const courseCreatorName = "Никита Михайлович";
+const reasonText = "потому что это востребованная и прибыльная профессия";
+const numberOfMonth = "3";
+
+let myInfoText = `Всем привет! Меня зовут ${myName}. 
+Я изучаю язык программирования ${progammingLanguage} на курсе 
+по ${progammingLanguage} у ${courseCreatorName}. 
+Я уверена, что пройду данный курс до конца!`;
+
+console.log(myInfoText);
---
+++ task4/ts2/index.js
@@ -0,0 +1,8 @@
+let myInfoText = `Всем привет! Меня зовут Юлия. Я изучаю язык программирования JavaScript на курсе по JavaScript у Никита Михайлович. Я уверена, что пройду данный курс до конца!`;
+
+let programmingLanguage = "JavaScript".toUpperCase;
+
+newText = myInfoText.replaceAll("JavaScript", programmingLanguage);
+
+console.log(myInfoText);
+console.log(newText);
