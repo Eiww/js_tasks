4611856c573ba14e66c68a4604a217a4e8b72e5c
Eiww <niktonikakoj0@gmail.com>
2024-02-19 21:10:20 +0900

    5_3 complete

---
+++ task5/ts3/index.js
@@ -0,0 +1,10 @@
+let questions = confirm(
+  "JavaScript появился в 1995 году?",
+  "Спецификация JavaScript называется ECMAScript?",
+  "JavaScript был создан за 1 месяц?"
+);
+if (questions === true) {
+  alert("Верно");
+} else {
+  alert("Неверно");
+}
