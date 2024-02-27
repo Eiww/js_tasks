dd012f0c82a52da75c70e75051fcba19e5ddc1ef
Eiww <niktonikakoj0@gmail.com>
2024-02-27 19:35:24 +0900

    6_2 complete

--- task6/ts2/index.js
+++ task6/ts2/index.js
@@ -1,1 +1,15 @@
-let getSumOfNumbers = (number, type);
+function getSumOfNumbers(number, type) {
+  let sum = 0;
+  for (i = 0; i <= number; i++) {
+    if (type === "odd" && i % 2 !== 0) {
+      sum += i;
+    } else if (type === "even" && i % 2 === 0) {
+      sum += i;
+    } else if (type === "") {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(getSumOfNumbers(10, "odd"));
